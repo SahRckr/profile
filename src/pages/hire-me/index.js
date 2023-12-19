020f6408ac3979606b24e9c37d78275f4a12f801
Sahil Satishkumar <sahilsatishkumar@gmail.com>
2023-12-19 17:24:11 +0530

    feat: update static pages
    - update books page to reflect recommended books & deprecate reading queue
    - enable hire me page
    - update resume

--- src/pages/hire-me/index.js
+++ src/pages/hire-me/index.js
@@ -6,25 +6,25 @@ import Head from "@docusaurus/Head";
 import styles from "./hire-me.module.css";
 
 const HireMe = () => (
-	<Layout>
-		<Head></Head>
-		<div className={styles.hiremeContainer}>
-			<h2>💼 Hire me:</h2>
-			<span>
-				I am looking for a new role and would appreciate your support. I am a
-				front-end engineer by experience, I have over 3 years of experience
-				building React apps, I have worked with GraphQL, AppSync, React-Native &
-				Redux/Redux-Saga.
-			</span>
-			<iframe
-				title="resume"
-				src="./Sahil_Satishkumar_2021.pdf#view=FitH&toolbar=0&navpanes=0&scrollbar=0"
-				importance="low"
-				loading="lazy"
-				frameBorder="0"
-			/>
-		</div>
-	</Layout>
+  <Layout>
+    <Head></Head>
+    <div className={styles.hiremeContainer}>
+      <h2>💼 Hire me:</h2>
+      <span>
+        I am looking for a new role and would appreciate your support. I am a
+        front-end engineer by experience, I have over 6 years of experience
+        building React apps, I have worked with GraphQL, AppSync, React-Native &
+        Redux/Redux-Saga.
+      </span>
+      <iframe
+        title="resume"
+        src="./Sahil_Satishkumar_2023.pdf#view=FitH&toolbar=0&navpanes=0&scrollbar=0"
+        importance="low"
+        loading="lazy"
+        frameBorder="0"
+      />
+    </div>
+  </Layout>
 );
 
 export default HireMe;
