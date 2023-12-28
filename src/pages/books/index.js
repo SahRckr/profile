138e6dc6a8cd89e1384daf339eaaa8ec365a8132
Sahil Satishkumar <sahilsatishkumar@gmail.com>
2023-12-29 01:21:53 +0530

    chore: books page, fix header rendering

--- src/pages/books/index.js
+++ src/pages/books/index.js
@@ -14,37 +14,37 @@ function Books() {
   return (
     <Layout title="Bookshelf">
       <div className={styles.bookshelf}>
+        <h2>
+          <a
+            rel="nofollow"
+            href="https://www.goodreads.com/review/list/73366453-sahil-satishkumar?shelf=currently-reading&utm_medium=api&utm_source=grid_widget"
+          >
+            Currently Reading:
+          </a>
+        </h2>
         <div id="gr_grid_widget_1587903274">
-          <h2>
-            <a
-              rel="nofollow"
-              href="https://www.goodreads.com/review/list/73366453-sahil-satishkumar?shelf=currently-reading&utm_medium=api&utm_source=grid_widget"
-            >
-              Currently Reading:
-            </a>
-          </h2>
           <div className="gr_grid_container" />
         </div>
+        <h2>
+          <a
+            rel="nofollow"
+            href="https://www.goodreads.com/review/list/73366453-sahil-satishkumar?shelf=must-read&utm_medium=api&utm_source=grid_widget"
+          >
+            Recommended:
+          </a>
+        </h2>
         <div id="gr_grid_widget_1702986163">
-          <h2>
-            <a
-              rel="nofollow"
-              href="https://www.goodreads.com/review/list/73366453-sahil-satishkumar?shelf=must-read&utm_medium=api&utm_source=grid_widget"
-            >
-              Recommended:
-            </a>
-          </h2>
           <div className="gr_grid_container" />
         </div>
+        <h2>
+          <a
+            rel="nofollow"
+            href="https://www.goodreads.com/review/list/73366453-sahil-satishkumar?shelf=read&utm_medium=api&utm_source=grid_widget"
+          >
+            Read:
+          </a>
+        </h2>
         <div id="gr_grid_widget_1587903573">
-          <h2>
-            <a
-              rel="nofollow"
-              href="https://www.goodreads.com/review/list/73366453-sahil-satishkumar?shelf=read&utm_medium=api&utm_source=grid_widget"
-            >
-              Read:
-            </a>
-          </h2>
           <div className="gr_grid_container" />
         </div>
       </div>
